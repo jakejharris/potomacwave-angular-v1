95eb4cb66a9eb85ff03cc092ee02dd8e3f26ebd7
jakejharris <h.jakej@gmail.com>
2024-06-27 18:56:54 -0400

    Prod ready maybe

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
   plugins: [angular()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
 });
